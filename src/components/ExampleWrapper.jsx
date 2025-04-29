bd69c1ed05250514ff94928ec6630dc6682eabb8
MonaRuz <mona.ruz@seznam.cz>
2025-04-29 22:41:13 +0200

    Created ExerciseWraper component

---
+++ src/components/ExampleWrapper.jsx
@@ -0,0 +1,11 @@
+import FirstNumber from "./FirstNumber";
+import ResultNumber from "./ResultNumber";
+import SecondNumer from "./SecondNumer";
+
+export default function ExampleWrapper() {
+	return <div>
+        <FirstNumber></FirstNumber>
+        <SecondNumer></SecondNumer>
+        <ResultNumber></ResultNumber>
+    </div>
+}
--- src/components/FirstNumber.jsx
+++ src/components/FirstNumber.jsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export default function FirstNumber() {
-  return (
-    <div>FirstNumber</div>
-  )
+	return <div>FirstNumber</div>
 }
